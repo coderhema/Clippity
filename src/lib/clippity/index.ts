c4c9701c56e132ee5d78abc76fd8c2f5a738e284
Olugbemi Tolulope <olugbemiopedepo@gmail.com>
2026-05-12 11:53:13 +0100

    Export Clippity runtime surface

---
+++ src/lib/clippity/index.ts
@@ -0,0 +1,6 @@
+export * from "./types";
+export * from "./capabilities";
+export * from "./beats-to-clips";
+export * from "./plugin-adapters";
+export * from "./orchestrator";
+export * from "./skills";
